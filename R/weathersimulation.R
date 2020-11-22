aeb9b25683918c1c0f75c2927899db7424ebfb1b
edemprd <defore2@vcu.edu>
2020-11-22 12:52:17 -0500

    Commit

--- R/weathersimulation.R
+++ R/weathersimulation.R
@@ -1,6 +1,15 @@
-
-
-# Question 4: Repeat this 1000 times and return the average of all these simulations.
+#' Weather Simulation (1000 times)
+#'
+#' Question 4: Repeat this 1000 times and return the average of all these simulations.
+#'
+#' @param N numeric positive integer
+#' @param initial numeric positive integer
+#' @param days numeric positive integer
+#' @export
+#' @author Edem Defor
+#' @examples
+#' weathersimulate(1000,1,10)
+#'
 
 
 
@@ -11,18 +20,18 @@ weathersimulate <- function (N, initial, days) {
   # Assume user defined conditions on the initial day
   mylist=mylist2=rep(list(initial),N)
 
-  set.seed(1998)
+  # set.seed(14528)
 
   for(j in 1:N){
     i=1
     while(i<days){                      # since the initial value is defined by the user, the while loop will run for days-1 times
       if  (y[i]==1) {
-        y[i+1] = rbinom(1,1,0.85)       # If a day is sunny, the probability that the next day is sunny is 0.85
-        if (y[i+1]==0) p=c(p,rexp(1,2))
+        y[i+1] = stats::rbinom(1,1,0.85)       # If a day is sunny, the probability that the next day is sunny is 0.85
+        if (y[i+1]==0) p=c(p,stats::rexp(1,2))
       }
       else{
-        y[i+1] = rbinom(1,1,0.65)       # If a day is rainy, the probability that the next day is sunny is 1-0.35=0.65, since the Y models number of sunny days in our function
-        if (y[i+1]==0) p=c(p,rexp(1,2))
+        y[i+1] = stats::rbinom(1,1,0.65)       # If a day is rainy, the probability that the next day is sunny is 1-0.35=0.65, since the Y models number of sunny days in our function
+        if (y[i+1]==0) p=c(p,stats::rexp(1,2))
       }
       mylist[[j]][i+1] = y[i+1]
       i=i+1
@@ -34,6 +43,3 @@ weathersimulate <- function (N, initial, days) {
 return (list("sunnydays"=sunny, "rainydays"=rainy, "meanaccum"=p))
 }
 
-nowe = weathersimulate(1000,1,10)
-
-str(nowe$meanaccum)
